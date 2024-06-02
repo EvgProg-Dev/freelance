855307255baf3de8599edc94ebe88b271c94cedb
EvgProg-Dev <152206681+EvgProg-Dev@users.noreply.github.com>
2024-06-02 14:21:02 +0300

    Add files via upload

---
+++ src/helpers/projectsList.js
@@ -0,0 +1,65 @@
+import project01 from './../img/projects/01.jpg';
+import project02 from './../img/projects/02.jpg';
+import project03 from './../img/projects/03.jpg';
+import project04 from './../img/projects/04.jpg';
+import project05 from './../img/projects/05.jpg';
+import project06 from './../img/projects/06.jpg';
+import project01big from './../img/projects/01-big.jpg';
+import project02big from './../img/projects/02-big.jpg';
+import project03big from './../img/projects/03-big.jpg';
+import project04big from './../img/projects/04-big.jpg';
+import project05big from './../img/projects/05-big.jpg';
+import project06big from './../img/projects/06-big.jpg';
+
+const projects = [
+    {
+        title: 'Gaming streaming portal',
+        skills: 'React, Node.js, MongoDB',
+        img: project01,
+        imgBig: project01big,
+        gitHubLink: '#!'
+    },
+
+    {
+        title: 'Video service',
+        skills: 'React, Node.js, MongoDB',
+        img: project02,
+        imgBig: project02big,
+        gitHubLink: '#!'
+    },
+
+    {
+        title: 'Video portal',
+        skills: 'React, Node.js, MongoDB',
+        img: project03,
+        imgBig: project03big,
+        gitHubLink: '#!'
+    },
+
+    {
+        title: 'Dating app',
+        skills: 'React, Node.js, MongoDB',
+        img: project04,
+        imgBig: project04big,
+        gitHubLink: '#!'
+    },
+
+    {
+        title: 'Landing',
+        skills: 'React, Node.js, MongoDB',
+        img: project05,
+        imgBig: project05big,
+        gitHubLink: '#!'
+    },
+
+    {
+        title: 'Gaming community',
+        skills: 'React, Node.js, MongoDB',
+        img: project06,
+        imgBig: project06big,
+
+    },
+];
+
+
+export {projects}
